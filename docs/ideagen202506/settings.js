99937f51c1b41a6e7ab5897c75c24df3849fe478
Yoshimasa Majima <majima.y@hokusei.ac.jp>
2025-06-22 11:02:32 +0900

    modify import/export statements

--- docs/ideagen202506/settings.js
+++ docs/ideagen202506/settings.js
@@ -1,1 +1,2 @@
-export const baseURL = 'https://jp.surveymonkey.com/r/YYT573P';
+const baseURL = 'https://jp.surveymonkey.com/r/YYT573P';
+export {baseURL};
--- docs/mysurvey.js
+++ docs/mysurvey.js
@@ -1,13 +1,13 @@
-export function getCcode(length = 10) {
+function getCcode(length = 10) {
     // 指定桁数（省略時デフォルト10桁）のランダムな整数を生成
     let randomCode = '';
     for (let i = 0; i < length; i++) {
         randomCode += Math.floor(Math.random() * 10);
     }
     return randomCode;
-}
+};
 
-export function getSurveyLink(baseURL, ccode) {
+function getSurveyLink(baseURL, ccode) {
     if (ccode === undefined || ccode === null || ccode === '') {
         // ccodeが未定義、null、または空文字の場合、デフォルトのURLを返す
         return baseURL;
@@ -15,4 +15,6 @@ export function getSurveyLink(baseURL, ccode) {
         // ccodeをURLに追加して返す
         return `${baseURL}?ccode=${ccode}`;
     }
-}
+};
+
+export {getCcode, getSurveyLink};
